3b986343f7f90b5d0ebd46f588b747a2552414c6
syafiranrdn <154555904+syafiranrdn@users.noreply.github.com>
2026-01-28 17:27:07 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const express = require("express");
-const mysql = require("mysql2/promise");
-const cors = require("cors");
-require("dotenv").config();
-
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-// TEST ROUTE
 app.get("/", (req, res) => {
-  res.json({ ok: true, message: "API running" });
+  res.json({
+    ok: true,
+    message: "API running (Node)"
+  });
 });
 
-// START SERVER
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
